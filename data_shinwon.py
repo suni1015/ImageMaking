716dfc139dc8e362361d04777bdabe04aba44f2c
suni1015 <jediandew@gmail.com>
2021-01-09 17:39:02 +0900

    first update

---
+++ data_shinwon.py
@@ -0,0 +1,28 @@
+# 신원 코드구분
+dic_codeFormat = {
+    'A': '브랜드코드',
+    'B': '시즌',
+    'C': '대복종',
+    'D': '소복종',
+    'E': '월',
+    '0000': '숫자4자리',
+    '0': '리오더'
+}
+
+# 브랜드
+dic_brand = {
+    "P": "SIEG",
+    "F": "FAHRENHEIT",
+    "B": "BESTIBELLI",
+    "S": "SI",
+    "V": "ISABEY",
+    "T": "VIKY",
+    "X": "SIWY",
+    "G": "GINNASIX",
+    "Q": "ICONIQ",
+    "M": "MarkM	"
+}
+
+
+
+
