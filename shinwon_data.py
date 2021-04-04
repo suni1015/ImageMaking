deb7ff43542288fc1c59531459223b7ff603b15d
L4mid <parkms5761@gmail.com>
2021-04-04 13:15:12 +0900

    merge file

--- shinwon_data.py
+++ shinwon_data.py
@@ -171,8 +171,10 @@ class Shinwon:
     str_poombun = None
     # df_product_info = None
     dic_product = {}  # 빈 딕셔너리 만들기. 의류정보 결과물을 저장할 공간이다.
+    dic_product_set = {}  # 빈 딕셔너리 만들기. 의류정보 결과물을 저장할 공간이다.  SET 용 공간
     size_count = None       # 이 제품의 사이즈 개수를 저장한다. (55사이즈, 66사이즈 --> 2개)
     list_poombun = None
+    bSET_poombun = False
 
     def __init__(self, ):
         return
@@ -253,6 +255,79 @@ class Shinwon:
         else:
             print('ERR : 월 코드')
 
+    def decode_poombun_set(self):
+        """
+        brand => self.str_poombun[0]
+        season => self.str_poombun[1]
+        category_big => self.str_poombun[2]
+        category_small => self.str_poombun[3]
+        month => self.str_poombun[4]
+        number => self.str_poombun[5:9]
+        reorder => self.str_poombun[10:]
+        """
+
+        # 브랜드
+        if self.str_poombun[0] in dic_brand:
+            # self.brand = dic_brand[self.str_poombun[0]]
+            self.dic_product_set['브랜드'] = dic_brand[self.str_poombun[0]]
+            # print('브랜드 : ', self.brand)
+        else:
+            print('ERR : 브랜드 코드')
+            #return -1, 'ERR : 브랜드 코드'
+
+        # 남성/여성 구분
+        if self.str_poombun[0] in set_man:
+            # self.sex = '남성'
+            self.dic_product_set['성별'] = '남성'
+            # print('성별 : ', self.sex)
+        elif self.str_poombun[0] in set_woman:
+            # self.sex = '여성'
+            self.dic_product_set['성별'] = '여성'
+            # print('성별 : ', self.sex)
+        else:
+            print('ERR : 성별1')
+
+        # 시즌
+        if self.str_poombun[1] in dic_season:
+            # self.season = dic_season[self.str_poombun[1]]
+            self.dic_product_set['시즌'] = dic_season[self.str_poombun[1]]
+            # print('시즌 : ', self.season)
+        else:
+            print('ERR : 시즌 코드')
+
+        # 대복종 (남성/여성, 액서서리/기타 구분)
+        '''품번 코드가 A로 시작하면 액서서리로서 코드가 2글자이며, 그외에는 1글자 코드 처리하면 된다.'''
+        tmp_code = self.str_poombun[2:4] if self.str_poombun[2] == 'A' else self.str_poombun[2]
+
+        if '성별' in self.dic_product_set:
+            if self.dic_product_set['성별'] == '남성':
+                if tmp_code in dic_man_category:
+                    # self.category_big = dic_man_category[tmp_code]
+                    self.dic_product_set['대복종'] = dic_man_category[tmp_code]
+                    # print('복종 : ', self.category_big)
+                else:
+                    print('ERR : 대복종 코드')
+
+            elif self.dic_product_set['성별'] == '여성':
+                if tmp_code in dic_woman_category:
+                    # self.category_big = dic_woman_category[tmp_code]
+                    self.dic_product_set['대복종'] = dic_woman_category[tmp_code]
+                    # print('복종 : ', self.category_big)
+                else:
+                    print('ERR : 대복종 코드')
+            else:
+                print('ERR : 성별2')
+
+        # 소복종
+
+        # 월
+        if self.str_poombun[4] in dic_month:
+            # self.month = dic_month[self.str_poombun[4]]
+            self.dic_product_set['월'] = dic_month[self.str_poombun[4]]
+            # print('월 : ', self.month)
+        else:
+            print('ERR : 월 코드')
+
     def print_poombun(self):
         print("품번 : ", self.str_poombun)
 
@@ -273,9 +348,8 @@ class Shinwon:
 
         self.df_all_data = pd.read_excel(str_file_path,  # write your directory here
                                          sheet_name=0,  # 시트이름을 쓰거나 index
-                                         # dtype={'sales_representative': np.int64,
-                                         #       'region': str,
-                                         #       'sales_amount': float},
+                                         #dtype={'기준\n사이즈': str},
+                                         #converters={'기준\n사이즈' : str},
                                          # names = ['region', 'sales_representative', 'sales_amount'],
                                          header=0,  # dictionary type
                                          # index_col=idx_col,
@@ -353,8 +427,89 @@ class Shinwon:
 
         return True
 
+
+    def parse_excel_data_set(self):
+        # 품번이 입력되었는지 예외처리할것
+        if not self.str_poombun:
+            print('ERR : 품번 정보가 없습니다.')
+            return False
+
+        df_all = self.df_all_data
+
+        # df 데이터 처리 테스트 코드
+        row_condition = df_all['품번']
+        #col_condition1 = ['브랜드', '관리코드', '상품분류코드', '상품명', '컬러', '시즌', '소재', '실측사이즈(cm)']
+        #col_condition2 = ['브랜드', '관리코드', '컬러', '시즌', '소재', '실측사이즈(cm)']
+        #df_product_info = df_all.loc[row_condition == self.str_poombun, col_condition2]  # 행/열 조건 삽입하여 데이터 추출
+
+        df_product_info = df_all[row_condition == self.str_poombun]  # 행 조건만 삽입하여 데이터 추출
+        #print(type(df_product_info.columns))
+
+        if df_product_info.empty:
+            print('ERR : 입력한 품번은 정보고시 파일에 존재하지 않습니다.')
+            return False
+        '''
+        품번
+        브랜드
+        분류코드
+        행사가
+    상품명
+    컬러명(한글 / 영문)
+        시즌
+    세탁방법
+    원산지
+    소재
+    사이즈
+    제조원
+    제조월
+        '''
+
+        self.dic_product_set['상품명'] = df_all.loc[row_condition == self.str_poombun, '상품명'].values[0]
+        self.dic_product_set['컬러명 ( 한글/영문 )'] = df_all.loc[row_condition == self.str_poombun, '컬러명 ( 한글/영문 )'].values[0]
+        self.dic_product_set['세탁방법'] = df_all.loc[row_condition == self.str_poombun, '세탁방법'].values[0]
+        self.dic_product_set['원산지'] = df_all.loc[row_condition == self.str_poombun, '원산지'].values[0]
+        self.dic_product_set['소재'] = df_all.loc[row_condition == self.str_poombun, '소재'].values[0]
+        #self.dic_product['상품특성'] = df_all.loc[row_condition == self.str_poombun, '상품특성'].values[0]
+        self.dic_product_set['제조원'] = df_all.loc[row_condition == self.str_poombun, '제조원'].values[0]
+        self.dic_product_set['제조월'] = df_all.loc[row_condition == self.str_poombun, '제조월'].values[0]
+        self.dic_product_set['사이즈'] = df_all.loc[row_condition == self.str_poombun, '사이즈'].values[0]
+        #self.dic_product['실측사이즈(cm)'] = df_all.loc[row_condition == self.str_poombun, '실측사이즈(cm)'].values[0]
+
+        # 칼럼 이름이 없어서 수작업으로 파싱.
+        '''
+        for i, item in enumerate(df_product_info.columns):
+            print(i, item)
+            if item == '상품특성':
+                col_idx1 = i + 1            # 상품특성의 값은 (엑셀상의) 다음칼럼에 있다.(칼럼 이름이 없어서 수작업 처리함)
+            elif item == '실측사이즈(cm)':
+                col_idx2 = i + 1            # 실측사이즈 값은 (엑셀상의) 다음칼럼에 있다.(칼럼 이름이 없어서 수작업 처리함)
+
+        print('상품특성의 값이 있는 칼럼 인덱스 : ', col_idx1)
+        print('실측사이즈 값이 있는 칼럼 인덱스 : ', col_idx2)
+        '''
+
+        #print(df_product_info.iloc[:, col_idx1].values[0])
+        #self.dic_product['상품특성 값'] = df_product_info.iloc[:, col_idx1].values[0]
+
+        # 사이즈 개수 세기
+        '''
+        token = ['/', ',']
+        self.size_count = 1     # 최소 1개이므로.
+        for tk in token:
+            self.size_count += self.dic_product['기준\n사이즈'].count(tk)
+        print('사이즈 개수 : ', self.size_count)
+
+        for i in range(self.size_count):
+            str_key = "사이즈%d" % i
+            self.dic_product[str_key] = df_product_info.iloc[:, col_idx2+i].values[0]
+        '''
+        return True
+
     def get_product_info(self):
-        return self.dic_product
+        if self.bSET_poombun == False:
+            return self.dic_product         # 일반 품번
+        else:
+            return self.dic_product_set     # Set 품번
 
     def clear_product_info(self):
         self.dic_product.clear()
